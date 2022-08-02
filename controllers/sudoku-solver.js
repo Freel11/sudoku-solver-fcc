828ad65107bc918e44ce5d71659bd6713c4aec5d
jade@itnt.co.za <jade@itnt.co.za>
2022-08-02 22:28:48 +0200

    solving the sudoku puzzle

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,6 +1,14 @@
 class SudokuSolver {
 
   validate(puzzleString) {
+    if (puzzleString.length != 81) {
+      return ({error: 'Expected puzzle to be 81 characters long'})
+    }
+    const regex = /[^0-9.]/
+    if (regex.test(puzzleString)) {
+      return ({error: 'Invalid characters in puzzle'})
+    }
+    return {}
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
@@ -15,9 +23,61 @@ class SudokuSolver {
 
   }
 
-  solve(puzzleString) {
-    
+  solve(grid) {
+
+    let newGrid = grid.concat()
+
+    for (let y = 0; y < 9; y++) {
+      for (let x = 0; x < 9; x++) {
+        if (newGrid[y][x] == '0') {
+          for (let n = 1; n < 10; n++) {
+            if (this.possible(newGrid, y, x, n)) {
+              newGrid[y][x] = n
+              if (this.solve(newGrid).varified) {
+                return {varified: true, grid: newGrid}
+              } else {
+                newGrid[y][x] = 0
+              }
+            }
+          }
+          return false
+        }
+      }
+    }
+    return {varified: true, grid: newGrid}
   }
+
+
+
+  possible(array, y, x, n) {
+
+    for (let i = 0; i < 9; i++) {
+      if (array[y][i] == n) {
+        return false
+      }
+    }
+
+    for (let i = 0; i < 9; i++) {
+      if (array[i][x] == n) {
+        return false
+      }
+    }
+
+    const x0 = (Math.floor(x/3)) * 3
+    const y0 = (Math.floor(y/3)) * 3
+
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (array[y0 + i][x0 + j] == n) {
+          return false
+        }
+      }
+    }
+
+    return true
+  }
+
+
 }
 
 module.exports = SudokuSolver;
--- routes/api.js
+++ routes/api.js
@@ -13,6 +13,33 @@ module.exports = function (app) {
     
   app.route('/api/solve')
     .post((req, res) => {
+      if (!req.body.hasOwnProperty('puzzle')) {
+        res.json({error: 'Required field missing'})
+        return
+      }
+      const puzzle = req.body.puzzle
+      const validation = solver.validate(puzzle)
 
+      if (Object.keys(validation)[0] == 'error') {
+        res.json(solver.validate(puzzle))
+        return
+      }
+
+      const string = puzzle.replaceAll('.','0')
+      const grid = []
+      string.split('').forEach((value, index) => {
+        if ((index + 1) % 9 == 0) {
+          grid.push(string.split('').slice((index + 1) - 9, index + 1))
+        }
+      })
+
+      let solution = solver.solve(grid)
+      if (!solution) {
+        res.json({error: 'Puzzle cannot be solved'})
+        return
+      }
+      solution = solution.grid
+      const joined = solution.flat(1)
+      res.json({solution: joined.join('')})
     });
 };
